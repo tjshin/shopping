781699c3a939c55fc3d2d2e888dd4e00060f8500
tjshin <dnltmzl0218@gmail.com>
2021-10-18 16:54:01 +0900

    spring boot shopping. semi-project start.2021.10.18

---
+++ src/main/webapp/WEB-INF/sql/order.sql
@@ -0,0 +1,46 @@
+CREATE TABLE cart(
+		cartno                        		NUMBER(10)		 NOT NULL		 PRIMARY KEY,
+		ID                            		VARCHAR2(10)		 NULL ,
+  FOREIGN KEY (ID) REFERENCES member (ID)
+);
+
+drop table orders cascade constraints;
+
+/**********************************/
+/* Table Name: 주문 */
+/**********************************/
+CREATE TABLE orders(
+orderno                        NUMBER(10)  NOT NULL  PRIMARY KEY,
+id                             VARCHAR2(10)  NOT NULL,
+contentsno                     NUMBER(10)  NULL ,
+cartno                         NUMBER(10)  NULL ,
+odate                          DATE  NOT NULL,
+quantity                       NUMBER(10)  NOT NULL,
+total                          NUMBER(10)  NOT NULL,
+payment                        VARCHAR2(50)  NOT NULL,
+mname                          VARCHAR2(20)  NOT NULL,
+pname                          VARCHAR2(50)  NOT NULL,
+  FOREIGN KEY (contentsno) REFERENCES contents (contentsno),
+  FOREIGN KEY (cartno) REFERENCES cart (cartno)
+);
+
+
+ 
+insert into orders(
+orderno, cartno, contentsno, odate, quantity, total, payment, id, mname, pname)
+values((select nvl(max(orderno),0)+1 from orders),null,5,sysdate,1,50000,'신용카드',
+'user4','홍길동','Ripped Skinny Jeans');
+ 
+insert into orders(
+orderno, cartno, contentsno, odate, quantity, total, payment, id, mname, pname)
+values((select nvl(max(orderno),0)+1 from orders),null,6,sysdate,1,50000,'신용카드',
+'user4','홍길동','Ripped Skinny Jeans');
+
+ 
+--(2) left outer join 문작성
+
+select  m.id, m.mname, m.fname, m.zipcode, m.address1, m.address2,
+         o.orderno,o.odate,o.pname,o.quantity,o.total,o.contentsno
+ from  member m left outer join orders o
+    on  m.id = o.id
+where  m.id='user4';
